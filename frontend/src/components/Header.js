a00340cce3d89c254c59423105237eac85278326
ushan pramod <ushandissanayaka879@gmail.com>
2024-06-03 07:21:28 +0530

    Header-updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Logo from './Logo'
 import { LuSearch } from "react-icons/lu";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -9,14 +11,28 @@ const Header = () => {
         <div className=''>
           <Logo w={90} h={50}/>
           </div>
-          <div className='flex items-center w-full justify-between max-w-sm'>
-            <input type='text' placeholder='search product here....' className='w-full'/>
+          <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-sm pl-2'>
+            <input type='text' placeholder='search product here....' className='w-full outline-none ' />
             <div className='text-lg min-w-[50px] w-13 h-8 bg-black flex items-center justify-center rounded-r-full text-white'>
               <LuSearch/>
              </div>
           </div>
-          <div>
-            user icons and card
+          <div className='flex item-center gap-5'>
+           <div className='text-3xl cursor-pointer'>
+            <FaRegCircleUser/>
+           </div>
+           <div className='text-2xl relative'>
+            <span>
+              <FaShoppingCart/>
+            </span>
+            <div className='bg-red-600  text-white w-5 h-5 rounded-full p-1 items-center flex justify-center absolute -top-2 -right-3'>
+              <p className='text-sm'>0</p>
+            </div>
+           </div>
+
+           <div>
+            <button className='px-2 py-1 rounded-full  bg-black text-white hover:bg-red-700'>Login</button>
+           </div>
           </div>
 
       </div>
