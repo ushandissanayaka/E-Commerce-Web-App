df09250bfbafba3eb61f5f7c8247119f101710c6
ushan pramod <ushandissanayaka879@gmail.com>
2024-06-03 17:28:12 +0530

    backend and database setup

---
+++ backend/controller/userSignUp.js
@@ -0,0 +1,13 @@
+
+async function userSignUpController(req,res){
+    try{
+
+    }catch(err){
+        res.json({
+            message : err,
+            error : true,
+            success : false,
+        })
+
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
+
+const app = express()
+app.use(cors())
+app.use("/api",router)
+
+const PORT = 8080 || process.env.PORT
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connected to DB")
+        console.log("server is running")
+    })
+
+})
